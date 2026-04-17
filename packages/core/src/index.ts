ac0ecc6b8d7422c7ca8a8128842a330bd1bfba95
mauricio del rio <mauricio.delr@gmail.com>
2026-04-17 01:59:09 -0400

    feat(core): export ThemeScope, ThemeScopeProps and FwTheme from package root

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -13,3 +13,8 @@ export * from "./tokens";
 // Utilities
 export { cn } from "./lib/cn";
 export { useDarkMode } from "./lib/useDarkMode";
+export {
+  ThemeScope,
+  type ThemeScopeProps,
+  type FwTheme,
+} from "./lib/ThemeScope";
